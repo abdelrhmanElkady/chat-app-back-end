dc74b220cf198a30dffb4a52b3bf5629740972b3
abdelrhmanElkady <48697496+abdelrhmanElkady@users.noreply.github.com>
2022-02-07 16:50:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -51,11 +51,11 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 ////for heroku
 //app.UseForwardedHeaders();
